400629fff0f2ae0a4c4ea376a1d249e73ade3ba4
Matt Thomas <matt@betweenbrain.com>
2017-01-01 20:18:05 -0500

    Adds MySQL example via --create cli argument

--- app.js
+++ app.js
@@ -10,17 +10,40 @@
 var headers;
 var i = 0;
 
+const mysql = require('./common/mysql');
 const readline = require('readline');
-const fs       = require('fs');
-const rl       = readline.createInterface({
-	input: fs.createReadStream('AccountHistory.csv')
+const fs = require('fs');
+const rl = readline.createInterface({
+    input: fs.createReadStream('AccountHistory.csv')
 });
 
 rl.on('line', function (line) {
-	if (i == 0) {
-		headers = line.split(',')
-	}
+    if (i == 0) {
+        headers = line.split(',');
+        console.log(headers);
+    }
 
-	// console.log(line);
-	i++;
+    i++;
 });
+
+if (process.argv.indexOf('--create') >= 0) {
+    var sql = 'CREATE DATABASE IF NOT EXISTS transactions ( ' +
+        '`date` DATE ' +
+        '`check` INT ' +
+        '`description` VARCHAR(255) ' +
+        '`debit` DECIMAL (8, 2) ' +
+        '`credit` DECIMAL (8,2) ' +
+        '`status` VARCHAR(156) ' +
+        '`balance` DECIMAL (12, 2) ' +
+        ') ENGINE=INNODB';
+
+    mysql.query(sql, null, function (err, rows) {
+        if(err){
+            console.log(err)
+        }
+
+        if(!err){
+            console.log(rows)
+        }
+    })
+}
