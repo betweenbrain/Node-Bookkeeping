8ccefe616bb421170d69bdafef5478ad7da8ae67
Matt Thomas <matt.thomas@searshc.com>
2017-01-14 16:50:19 -0500

    Adds basic list view

---
+++ models/listModel.js
@@ -0,0 +1,26 @@
+/**
+ * Project    Node-Bookkeeping
+ * File       listModel.js
+ * Created    1/14/17 4:11 PM
+ * Author     Matt Thomas
+ * Email      matt.thomas@searshc.com
+ * Copyright  Copyright (C) 2017 Sears Holdings. All Rights Reserved.
+ */
+
+const mysql = require('../common/mysql');
+
+module.exports = {
+    list: function (callback) {
+        var sql = 'SELECT date, description, amount, category FROM transactions';
+
+        mysql.query(sql, null, function (err, rows) {
+            if (err) {
+                callback(err)
+            }
+
+            if (!err) {
+                callback(null, rows)
+            }
+        })
+    }
+}
---
+++ routes/list.js
@@ -0,0 +1,24 @@
+/**
+ * Project    Node-Bookkeeping
+ * File       list.js
+ * Created    1/14/17 4:11 PM
+ * Author     Matt Thomas
+ * Email      matt.thomas@searshc.com
+ * Copyright  Copyright (C) 2017 Sears Holdings. All Rights Reserved.
+ */
+
+var listModel = require('../models/listModel');
+
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+        listModel.list(function (err, data) {
+            if (err) {
+                res.status(400);
+                res.render('index', {message: err});
+            }
+            if (!err) {
+                res.render('list', {rows: data});
+            }
+        })
+    });
+};
