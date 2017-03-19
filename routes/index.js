78358b3717671ec58d2e3fa7f5dca7bf0094166f
Matt Thomas <matt.thomas@searshc.com>
2017-03-19 16:30:54 -0400

    Implements index view with trans by category

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,40 @@
  * Copyright  Copyright (C) 2017 Sears Holdings. All Rights Reserved.
  */
 
+var listModel = require('../models/listModel');
+var categories;
+
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        res.render('index', {message: 'Welcome!'});
+        listModel.categories(function (err, rows) {
+            categories = (!err)
+                ? rows
+                : null;
+        });
+
+        var links = [
+            {
+                href: '/import',
+                text: 'Import transactions'
+            },
+            {
+                href: '/list',
+                text: 'List all transactions'
+            },
+            {
+                href: '/list/category/',
+                text: 'List all transactions by category',
+                cats: categories
+            },
+            {
+                href: '/summary',
+                text: 'Display category balances'
+            }
+        ];
+
+        res.render('index', {
+            links  : links,
+            message: 'Welcome!'
+        });
     })
-}
+};
